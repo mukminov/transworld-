78fc17fcbfee57d23851298c7f3abb92cf3bc93e
Mukminov Timur <mukminov.timur@list.ru>
2017-11-18 18:01:54 +0300

    index.js

---
+++ index.js
@@ -0,0 +1,25 @@
+window.onload = function(){
+	var slides = document.querySelectorAll( " .slides .aside-block ");
+	var i = 0;
+
+	document.getElementById("btn-prev").onclick = function(){
+		slides[i].className = "" ;
+		i--;
+
+		if (i<0){
+			i = slides.length - 1 ;
+		}
+		slides[i].className = "active";
+	}
+
+	document.getElementById("btn-next").onclick = function(){
+		slides[i].className = "" ;
+		i++;
+
+		if (i>= slides.length){
+			i = 0 ;
+			}
+		slides[i].className = "active";
+	}
+}
+	
--- js
+++
@@ -1,6 +0,0 @@
-document.getElementById('slider-left').onclick = slider-left;
-
-function slider-left(argument) {
-	var slides = getElementId('slides ');
-	slides.style.left= 320 +"px" // body...
-}
